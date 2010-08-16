6fc0872ebe49d322e678f46db6f8c3f9cc2cf73c
Kazimierz Pogoda <hshsce@gmail.com>
2010-08-16 23:57:43 +0200

    new tadedon-configuration module

---
+++ tadedon-configuration/src/main/java/com/xemantic/tadedon/configuration/Configurations.java
@@ -0,0 +1,191 @@
+/*
+ * Copyright 2010 Xemantic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.xemantic.tadedon.configuration;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.FileConfiguration;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Charsets;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.io.Resources;
+import com.xemantic.tadedon.guava.base.MoreResources;
+
+/**
+ * Utilities supporting commons-configuration library.
+ * <p>
+ * Created on Aug 15, 2010
+ *
+ * @author hshsce
+ */
+public final class Configurations {
+
+    public static final String DEFAULT_CONFIGURATION_PATH = "META-INF/default-configuration";
+
+    public static final String DEFAULT_CONFIGURATION_LIST = DEFAULT_CONFIGURATION_PATH + "/configuration.list";
+
+    private static final Logger LOG = LoggerFactory.getLogger(Configurations.class);
+
+
+    private Configurations() { /* util class, non-instantiable */ }
+
+    /**
+     * Returns configuration for given {@code confDir} and {@code confName}.
+     *
+     * @param confDir the configuration directory.
+     * @param confName the configuration file name.
+     * @return the configuration.
+     * @throws ConfigurationException 
+     */
+    public static FileConfiguration getConfiguration(File confDir, String confName) {
+        return getConfiguration(new File(confDir, confName));
+    }
+
+    public static FileConfiguration getConfiguration(File confFile) {
+        String confName = confFile.getName();
+        FileConfiguration conf;
+        if (confName.endsWith(".properties")) {
+            conf = newPropertiesConfiguration(confFile);
+        } else if (confName.endsWith(".xml")) {
+            conf = newXmlConfiguration(confFile);            
+        } else {
+            throw new IllegalArgumentException(
+                    "Unknown configuration file: " + confName + " it should have either " +
+                    ".properties or .xml extension");
+        }
+        return conf;
+    }
+
+    public static XMLConfiguration newXmlConfiguration(File confFile) {
+        try {
+            return new XMLConfiguration(confFile);
+        } catch (ConfigurationException e) {
+            throw new RuntimeException("Cannot create configuration", e);
+        }
+    }
+
+    public static PropertiesConfiguration newPropertiesConfiguration(File confFile) {
+        try {
+            return new PropertiesConfiguration(confFile);
+        } catch (ConfigurationException e) {
+            throw new RuntimeException("Cannot create configuration", e);
+        }
+    }
+
+    public static PropertiesConfiguration newPropertiesConfiguration(URL confUrl) {
+        try {
+            return new PropertiesConfiguration(confUrl);
+        } catch (ConfigurationException e) {
+            throw new RuntimeException("Cannot create configuration", e);
+        }
+    }
+
+    public static void mergeAllDefaults(File confDir) {
+        LOG.info("Merging default configurations from classpath with configuraiton dir: {}", confDir);
+        Set<String> defaultConfigurations = Configurations.getDefaultConfigurations();
+        LOG.debug("Default configuration sources: {}", defaultConfigurations);
+        for (String confName : defaultConfigurations) {
+            mergeDefaults(confDir, confName);
+        }
+    }
+
+    public static void mergeDefaults(File confDir, String confName) {
+        LOG.debug("Merging configuration: {}", confName);
+        URL url = Configurations.getDefaultConfigurationUrl(confName);
+        File confFile = new File(confDir, confName);
+        if (confFile.exists()) {
+            Configurations.merge(url, confFile);
+        } else {
+            MoreResources.copyRisky(url, confFile);                
+        }        
+    }
+
+    public static URL getDefaultConfigurationUrl(String configName) {
+        return Resources.getResource(DEFAULT_CONFIGURATION_PATH + "/" + configName);
+    }
+
+    public static Set<String> getDefaultConfigurations() {
+        ImmutableSet.Builder<String> builder = ImmutableSet.<String>builder();
+        List<URL> configurationLists = MoreResources.getResources(DEFAULT_CONFIGURATION_LIST);
+        for (URL url : configurationLists) {
+            builder.addAll(getLines(url));
+        }
+        return builder.build();
+    }
+
+    public static List<String> getLines(URL url) {
+        try {
+            return Resources.readLines(url, Charsets.UTF_8);
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot read lines from url: " + url, e);
+        }        
+    }
+
+    public static void merge(URL defaultConfUrl, File confFile) {
+        LOG.debug("Merging {} <-> {}", defaultConfUrl, confFile);
+        String confName = confFile.getName();
+        if (confName.endsWith(".xml")) {
+            LOG.warn("Merging of XML files is unsupported at the moment, " +
+            		 "you have to adjust configuration file manually: {}", confFile);
+        } else if (confName.endsWith(".properties")) {
+            merge(newPropertiesConfiguration(defaultConfUrl), newPropertiesConfiguration(confFile));
+        }
+    }
+
+    public static void merge(PropertiesConfiguration defaultConf, PropertiesConfiguration conf){
+        LOG.debug("Merging configurations: {} <-> {}", defaultConf.getURL(), conf.getURL());
+        for (@SuppressWarnings("unchecked") Iterator<String> iterator = defaultConf.getKeys(); iterator.hasNext();) {
+            String key = iterator.next();
+            if (!conf.containsKey(key)) {
+                copyProperty(key, defaultConf, conf);
+            }
+        }
+        try {
+            conf.save();
+        } catch (ConfigurationException e) {
+            throw new RuntimeException("Could no save configuration file: " + conf.getFileName(), e);
+        }
+    }
+
+    /**
+     * Copies property identified by {@code key} from the {@code srcConf} to {@code dstConf}.
+     * This method will also copy property comments.
+     *
+     * @param defaultConf
+     * @param conf
+     * @param key
+     */
+    public static void copyProperty(String key, PropertiesConfiguration srcConf, PropertiesConfiguration dstConf) {
+        LOG.debug("Copying property: {}", key);
+        Object value = srcConf.getProperty(key);
+        dstConf.setProperty(key, value);
+        String comment = srcConf.getLayout().getComment(key);
+        if (comment != null) {
+            dstConf.getLayout().setComment(key, comment);
+        }
+    }
+
+}
---
+++ tadedon-configuration/src/test/java/com/xemantic/tadedon/configuration/ConfigurationsTest.java
@@ -0,0 +1,166 @@
+/*
+ * Copyright 2010 Xemantic
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.xemantic.tadedon.configuration;
+
+import static com.xemantic.tadedon.configuration.ConfigurationTests.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration.XMLConfiguration;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.google.common.io.Files;
+
+/**
+ * Unit test for {@link Configurations}.
+ * <p>
+ * Created on Aug 16, 2010
+ *
+ * @author hshsce
+ */
+public class ConfigurationsTest {
+
+    private File testConfDir = new File("target/test-conf-dir");
+
+    @Before
+    public void createTestConfDir() throws IOException {
+        if (!testConfDir.mkdir()) {
+            throw new IOException("Cannot create directory: " + testConfDir);
+        }
+    }
+
+    @Test
+    public void shouldCopyPropertyWithComments() throws IOException, ConfigurationException {
+        // given
+        File confFile1 = new File("src/test/data/conf1.properties");
+        File confFile2 = new File("src/test/data/conf2.properties");
+        File confFile3 = new File("src/test/data/conf3.properties");
+        File outConfFile = new File(testConfDir, "outConf.properties");
+        Files.copy(confFile1, outConfFile);
+        PropertiesConfiguration conf2 = new PropertiesConfiguration(confFile2);
+        PropertiesConfiguration outConf = new PropertiesConfiguration(outConfFile);
+
+        // when
+        Configurations.copyProperty("bar", conf2, outConf);
+        outConf.save();
+
+        // then
+        assertThat(Files.equal(outConfFile, confFile3), is(true));
+    }
+
+    /**
+     * copying of XML properties does not work at the moment
+     * @throws IOException
+     * @throws ConfigurationException
+     */
+    @Test
+    @Ignore
+    public void shouldCopyXmlPropertyWithoutComments() throws IOException, ConfigurationException {
+        // given
+        File confFile1 = new File("src/test/data/conf1.xml");
+        File confFile2 = new File("src/test/data/conf2.xml");
+        File confFile3 = new File("src/test/data/conf3.xml");
+        File outConfFile = new File(testConfDir, "outConf.xml");
+        Files.copy(confFile1, outConfFile);
+        XMLConfiguration conf2 = new XMLConfiguration(confFile2);
+        XMLConfiguration outConf = new XMLConfiguration(outConfFile);
+
+        // when
+        //Configurations.copyProperty("bar", conf2, outConf);
+        outConf.save();
+
+        // then
+        assertThat(Files.equal(outConfFile, confFile3), is(true));
+    }
+
+    @Test
+    public void shouldCopyAllConfigurationsOnMergeAllDefaultsIfOutputDirIsEmpty() throws IOException {
+        // given classpath with default configuration 
+
+        // when
+        Configurations.mergeAllDefaults(testConfDir);
+
+        // then
+        assertThat(testConfDir.list().length, is(4));
+        assertThat(new File(testConfDir, "Component1.properties").exists(), is(true));
+        assertThat(new File(testConfDir, "Component2.properties").exists(), is(true));
+        assertThat(new File(testConfDir, "Component3.xml").exists(), is(true));
+        assertThat(new File(testConfDir, "Component4.xml").exists(), is(true));
+        assertThat(Files.equal(new File(testConfDir, "Component1.properties"), getDefaultConfFile("Component1.properties")), is(true));
+        assertThat(Files.equal(new File(testConfDir, "Component2.properties"), getDefaultConfFile("Component2.properties")), is(true));
+        assertThat(Files.equal(new File(testConfDir, "Component3.xml"), getDefaultConfFile("Component3.xml")), is(true));
+        assertThat(Files.equal(new File(testConfDir, "Component4.xml"), getDefaultConfFile("Component4.xml")), is(true));
+    }
+
+    @Test
+    public void shouldAddOneMissingConfigurationFileOnMergeAllDefaults() throws IOException {
+        // given classpath with default configuration and 
+        Files.copy(getDefaultConfFile("Component1.properties"), new File(testConfDir, "Component1.properties"));
+        Files.copy(getDefaultConfFile("Component2.properties"), new File(testConfDir, "Component2.properties"));
+        Files.copy(getDefaultConfFile("Component3.xml"), new File(testConfDir, "Component3.xml"));
+
+        // when
+        Configurations.mergeAllDefaults(testConfDir);
+
+        // then
+        assertThat(testConfDir.list().length, is(4));
+        assertThat(new File(testConfDir, "Component1.properties").exists(), is(true));
+        assertThat(new File(testConfDir, "Component2.properties").exists(), is(true));
+        assertThat(new File(testConfDir, "Component3.xml").exists(), is(true));
+        assertThat(new File(testConfDir, "Component4.xml").exists(), is(true));
+        assertThat(Files.equal(new File(testConfDir, "Component1.properties"), getDefaultConfFile("Component1.properties")), is(true));
+        assertThat(Files.equal(new File(testConfDir, "Component2.properties"), getDefaultConfFile("Component2.properties")), is(true));
+        assertThat(Files.equal(new File(testConfDir, "Component3.xml"), getDefaultConfFile("Component3.xml")), is(true));
+        assertThat(Files.equal(new File(testConfDir, "Component4.xml"), getDefaultConfFile("Component4.xml")), is(true));
+    }
+
+    @Test
+    public void shouldUpdateOneConfigurationFileOnMergeAllDefaults() throws IOException {
+        // given classpath with default configuration and 
+        Files.copy(getDefaultConfFile("Component1.properties"), new File(testConfDir, "Component1.properties"));
+        Files.copy(getDefaultConfFile("Component1.properties"), new File(testConfDir, "Component2.properties"));
+        Files.copy(getDefaultConfFile("Component3.xml"), new File(testConfDir, "Component3.xml"));
+        Files.copy(getDefaultConfFile("Component4.xml"), new File(testConfDir, "Component4.xml"));
+
+        // when
+        Configurations.mergeAllDefaults(testConfDir);
+
+        // then
+        assertThat(testConfDir.list().length, is(4));
+        assertThat(new File(testConfDir, "Component1.properties").exists(), is(true));
+        assertThat(new File(testConfDir, "Component2.properties").exists(), is(true));
+        assertThat(new File(testConfDir, "Component3.xml").exists(), is(true));
+        assertThat(new File(testConfDir, "Component4.xml").exists(), is(true));
+        assertThat(Files.equal(new File(testConfDir, "Component1.properties"), getDefaultConfFile("Component1.properties")), is(true));
+        assertThat(Files.equal(new File(testConfDir, "Component2.properties"), new File("src/test/data/Component2-updated.properties")), is(true));
+        assertThat(Files.equal(new File(testConfDir, "Component3.xml"), getDefaultConfFile("Component3.xml")), is(true));
+        assertThat(Files.equal(new File(testConfDir, "Component4.xml"), getDefaultConfFile("Component4.xml")), is(true));
+    }
+
+    @After
+    public void deleteTestConfDir() throws IOException {
+        Files.deleteRecursively(testConfDir);
+    }
+
+}
