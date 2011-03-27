bc8a4c37e1dfddf9d22c08168d6769a5137ff983
morisil <morisil@xemantic.com>
2011-03-27 23:48:24 +0200

    LoggerModule now injects logger named after the type to which the logger is injected

--- tadedon-guice-logging/src/test/java/com/xemantic/tadedon/guice/logging/LoggerModuleTest.java
+++ tadedon-guice-logging/src/test/java/com/xemantic/tadedon/guice/logging/LoggerModuleTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 Xemantic
+ * Copyright 2010-2011 Xemantic
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,31 +15,64 @@
  */
 package com.xemantic.tadedon.guice.logging;
 
+import static org.mockito.Mockito.*;
+
 import org.junit.Test;
+import org.slf4j.ILoggerFactory;
+import org.slf4j.Logger;
+import org.slf4j.impl.StaticLoggerBinder;
 
 import com.google.inject.Guice;
+import com.google.inject.Inject;
 import com.google.inject.Injector;
 
 /**
- * 
+ * Test for {@link LoggerModule}.
  * <p>
  * Created on Aug 16, 2010
  *
- * @author hshsce
+ * @author morisil
  */
 public class LoggerModuleTest {
 
     @Test
-    public void shouldInjectLogger() {
-        // given injector
+    public void shouldInjectLogger() throws InterruptedException {
+        // given
+        final Logger defaultLogger = mock(Logger.class);
+        final Logger serviceLogger = mock(Logger.class);
+        StaticLoggerBinder.setLoggerFactory(new ILoggerFactory() {
+            @Override
+            public Logger getLogger(String name) {
+                if (LoggingService.class.getName().equals(name)) {
+                    return serviceLogger;
+                }
+                return defaultLogger;
+            }
+        });
         Injector injector = Guice.createInjector(new LoggerModule());
+        LoggingService loggingService = injector.getInstance(LoggingService.class);
 
         // when
-        LoggingService loggingService = injector.getInstance(LoggingService.class);
-        loggingService.log();
+        loggingService.logInfoMessage();
 
         // then
-        //TODO verify that it was really logged.
+        verifyZeroInteractions(defaultLogger);
+        verify(serviceLogger).info("logging info message");
+    }
+
+    public static class LoggingService {
+
+        private final Logger logger;
+
+        @Inject
+        public LoggingService(Logger logger) {
+            this.logger = logger;
+        }
+
+        public void logInfoMessage() {
+            logger.info("logging info message");
+        }
+
     }
 
 }
